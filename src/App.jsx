b8c192d6075c96a147c25e9173de05ca26f0f1de
shadilvp <shadilx8@gmail.com>
2024-10-16 12:39:51 +0530

    created a new folder and pushed the all files of user page into that folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,17 @@
 
 import './App.css'
 import { Route, Routes } from 'react-router-dom'
-
-import Home from './Components/pagas/Home'
-import About from './Components/pagas/About'
-import Contact from './Components/pagas/Contact'
-import LogIn from './Components/LogIn-SignIn/LogIn'
-import SignUp from './Components/LogIn-SignIn/SignUp'
-import Cart from './Components/pagas/Cart'
-import Shop from './Components/pagas/Shop'
-import Context from './Hooks/Context'
-import ShowItem from './Components/pagas/ShowItems'
-import ProceedPayment from './Components/pagas/ProceedPayment'
-import UserDetails from './Components/pagas/UserDetails'
+import Home from './User/Components/pagas/Home'
+import About from './User/Components/pagas/About'
+import Contact from './User/Components/pagas/Contact'
+import LogIn from './User/Components/LogIn-SignIn/LogIn'
+import SignUp from './User/Components/LogIn-SignIn/SignUp'
+import Cart from './User/Components/pagas/Cart'
+import Shop from './User/Components/pagas/Shop'
+import Context from './User/Hooks/Context'
+import ShowItem from './User/Components/pagas/ShowItems'
+import ProceedPayment from './User/Components/pagas/ProceedPayment'
+import UserDetails from './User/Components/pagas/UserDetails'
 
 
 function App() {
